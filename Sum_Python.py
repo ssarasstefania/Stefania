b6093ea652a8e7e11b078dbd1b9e999c8eb458b1
ssarasstefania <135132245+ssarasstefania@users.noreply.github.com>
2023-05-31 08:30:50 -0500

    Update Sum_Python.py

--- Sum_Python.py
+++ Sum_Python.py
@@ -1,1 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>¡Bienvenido!</title>
+</head>
+<body>
+  <h1>¡Hola!</h1>
+  <img src="ruta_de_tu_imagen.jpg" alt="Descripción de la imagen">
+</body>
+</html>
 
